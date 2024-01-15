358963d3eae783ed790b92bc6908bff71a50210b
Delvon Anderson <130073814+dnaDeveloper2@users.noreply.github.com>
2024-01-14 20:08:52 -0500

    Delete src directory

--- src/reviews/reviews.router.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const reviewsController = require('./reviews.controller');
-const methodNotAllowed = require('../errors/methodNotAllowed');
-
-router.route('/:reviewId').put(reviewsController.updateReview).delete(reviewsController.deleteReview).all(methodNotAllowed);
-
-module.exports = router;
