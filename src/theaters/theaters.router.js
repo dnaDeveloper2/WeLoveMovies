33452f1dac401aa9c8b3a87c76099bf9a817947f
Delvon Anderson <130073814+dnaDeveloper2@users.noreply.github.com>
2024-01-14 19:55:59 -0500

    Delete src directory

--- src/theaters/theaters.router.js
+++
@@ -1,9 +0,0 @@
-// // theaters.router.js
-// const express = require('express');
-// const router = express.Router();
-// const theatersController = require('./theaters.controller');
-
-// router.get('/theaters', theatersController.getAllTheaters);
-// // Define other theaters routes...
-
-// module.exports = router;
